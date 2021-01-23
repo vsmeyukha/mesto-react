e7050feb957c92e35d97c3bd19855dacb1686523
vsmeyukha <vsmeyukha@gmail.com>
2021-01-24 02:39:18 +0300

    fix: исправлена мнемоника копирайта в футере

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__info">&COPY; 2020 Mesto Russia. Dev: Victor Smeyukha</p>
+      <p className="footer__info">&copy;	 2020 Mesto Russia. Dev: Victor Smeyukha</p>
     </footer>
   )
 }
